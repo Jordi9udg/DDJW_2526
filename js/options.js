e91bf56a98d0b63e488b5157cbece6827178520d
Antonio Rodrigues Benítez <antonio.rodriguez@udg.edu>
2026-03-05 14:02:42 +0100

    Tractem opcions a JS

--- js/options.js
+++ js/options.js
@@ -1,12 +1,31 @@
 import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
+const default_options = {
+    pairs: 2,
+    difficulty: 'normal'
+} 
+
 var pairs = $('#pairs');
 var difficulty = $('#dif');
 
+var options = Object.create(default_options);
+
 pairs.on('change', function (){
-    console.log(pairs.val());
+    options.pairs = pairs.val();
 });
 
 difficulty.on('change', function (){
-    console.log(difficulty.val());
+    options.difficulty = difficulty.val();
+});
+
+$('#default').on('click', function(){
+    options.pairs = default_options.pairs;
+    options.difficulty = default_options.difficulty;
+    pairs.val(options.pairs);
+    difficulty.val(options.difficulty);
+})
+
+$('#apply').on('click', function(){
+    // TODO: Save
+    location.assign("../");
 });
