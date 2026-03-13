a9f6c06602a217b38eca972be257325e66722d28
Antonio Rodrigues Benítez <antonio.rodriguez@udg.edu>
2026-03-13 11:27:33 +0100

    Carregar textures

--- js/canvasgame.js
+++ js/canvasgame.js
@@ -3,6 +3,8 @@ import {clickCard, gameItems, selectCards, startGame, initCard, saveGame} from "
 
 let game = $('#game');
 let canvas = game[0].getContext('2d');
+let resources = {};
+let cards = [];
 if (canvas){
     game.attr("width", 800);
     game.attr("height", 600);
@@ -11,12 +13,29 @@ if (canvas){
 }
 
 function start(){
-    // TODO: Carregar recursos
+    selectCards();
+    cards = [...gameItems];
+    loadCardResource("../resources/back.png");
+    cards.forEach(card => {
+        loadCardResource(card);
+        initCard(val => card = val);
+    });
     // TODO: Vincular events
+    startGame();
 }
 
 function update(){
-    checkInput();
-    draw();
+//    checkInput();
+//    draw();
     // TODO: Com es pot fer per que això sigui LOOP
+}
+
+function loadCardResource(src){
+    if (!resources[src]){
+        let res = {image: null, ready: false}
+        res.image = new Image();
+        res.image.src = src;
+        res.image.onload = ()=> res.ready = true;
+        resources[src] = res;
+    }
 }
